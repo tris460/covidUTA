e0887d6e49b9e1d090cd608e55379b1232f1ac77
Bety Martinez <trism460@gmail.com>
2022-03-27 16:31:32 -0600

    Initial design for the api-data

--- src/app/api-data/api-data.page.ts
+++ src/app/api-data/api-data.page.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-data.page.scss'],
 })
 export class ApiDataPage implements OnInit {
+  logo: string;
+  background: string;
 
-  constructor() { }
+  constructor() {
+    this.logo = '../../assets/logocovid.png';
+    this.background = '../../assets/background1.jpeg';
+  }
 
   ngOnInit() {
   }
