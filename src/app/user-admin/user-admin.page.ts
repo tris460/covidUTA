98071e1704865541cf96e62c73a42342e35f72c4
Bety Martinez <trism460@gmail.com>
2022-03-27 19:10:57 -0600

    Initial design for home-admin

--- src/app/user-admin/user-admin.page.ts
+++ src/app/user-admin/user-admin.page.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-admin.page.scss'],
 })
 export class UserAdminPage implements OnInit {
+  logo: string;
+  background: string;
 
-  constructor() { }
+  constructor() {
+    this.logo = '../../assets/logocovid.png';
+    this.background = '../../assets/background1.jpeg';
+  }
 
   ngOnInit() {
   }
