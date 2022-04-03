99a289678afa5c44b40b48c71a29d9f05a9770a4
Bety Martinez <trism460@gmail.com>
2022-04-03 16:14:18 -0500

    Show illness for each account

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiIllnessService } from '../services/api-illness.service';
+import { UserDataService } from '../services/user-data.service';
 
 @Component({
   selector: 'app-history',
@@ -9,10 +10,29 @@ import { ApiIllnessService } from '../services/api-illness.service';
 export class HistoryPage implements OnInit {
   background: string;
   logo: string;
+  userName: object;
+  date: string;
+  illness: string;
+  symptoms: Array<string>;
+  status: number;
+  notes: string;
+  illnessData: Array<any>;
 
-  constructor(private apiIllness: ApiIllnessService) {
+  constructor(public userDataService: UserDataService, private apiIllness: ApiIllnessService) {
     this.logo = '../../assets/logocovid.png';
     this.background = '../../assets/background1.jpeg';
+    this.userName = userDataService.userLogged.email;
+    this.getIllnesses();
+    this.date = '';
+    this.illness = '';
+    this.symptoms = [];
+    this.status = 0;
+    this.notes = '';
+  }
+  getIllnesses() { // Get the illnesses of the logged user
+    this.illnessData = this.apiIllness.information;
+    this.illnessData = this.illnessData.filter(u =>
+      u.idUser === this.userName);
   }
   getData() {
     console.log('Click');
