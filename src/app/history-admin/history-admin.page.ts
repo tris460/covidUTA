5e6f67eca9147e745d6ee745e79eeebfd0987e3f
Bety Martinez <trism460@gmail.com>
2022-04-05 08:19:58 -0500

    Get the options of the select

--- src/app/history-admin/history-admin.page.ts
+++ src/app/history-admin/history-admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiIllnessService } from '../services/api-illness.service';
 
 @Component({
   selector: 'app-history-admin',
@@ -8,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class HistoryAdminPage implements OnInit {
   background: string;
   logo: string;
+  illness: Array<any>;
 
-  constructor() {
+  constructor(public apiIllness: ApiIllnessService) {
     this.logo = '../../assets/logocovid.png';
     this.background = '../../assets/background1.jpeg';
+    this.getIllnesses();
+   }
+   getIllnesses() { //Get the illnesses registered
+    setTimeout(() => {
+      this.illness = this.apiIllness.information;
+    }, 50);
    }
   ngOnInit() {
   }
