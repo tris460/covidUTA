f77279cc0f20c9286775cd79950f0387cb414e38
Bety Martinez <trism460@gmail.com>
2022-04-05 08:46:23 -0500

    Show info of the user selected

--- src/app/history-admin/history-admin.page.ts
+++ src/app/history-admin/history-admin.page.ts
@@ -10,20 +10,33 @@ export class HistoryAdminPage implements OnInit {
   background: string;
   logo: string;
   illness: Array<any>;
+  data: any;
+  dateData: string;
+  illnessData: string;
+  symptomsData: Array<string>;
+  statusData: string;
+  notesData: string;
 
   constructor(public apiIllness: ApiIllnessService) {
     this.logo = '../../assets/logocovid.png';
     this.background = '../../assets/background1.jpeg';
     this.getIllnesses();
-   }
-   getIllnesses() { //Get the illnesses registered
+    this.notesData = '';
+  }
+  getIllnesses() { //Get the illnesses registered
     setTimeout(() => {
       this.illness = this.apiIllness.information;
     }, 50);
-   }
-   showData($event) {
-     console.log($event.target.value);
-   }
+  }
+  showData($event) {
+    this.data = $event.target.value;
+    const dataInformation = this.illness[this.data];
+    this.dateData = dataInformation.strDate.slice(0,10);
+    this.illnessData = dataInformation.strName;
+    this.symptomsData = dataInformation.arrSymptoms;
+    this.statusData = dataInformation.intStatus;
+    this.notesData = dataInformation.strNotes;
+  }
   ngOnInit() {
   }
 
