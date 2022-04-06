812aabe40f76021e6dcc218f5d732a55ffb2405b
Bety Martinez <trism460@gmail.com>
2022-04-06 15:57:52 -0500

    Get admin information

--- src/app/history-superadmin/history-superadmin.page.ts
+++ src/app/history-superadmin/history-superadmin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiUserService } from '../services/api-user.service';
 
 @Component({
   selector: 'app-history-superadmin',
@@ -8,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class HistorySuperadminPage implements OnInit {
   logo: string;
   background: string;
+  admins: Array<any>;
 
-  constructor() {
+  constructor(private apiUser: ApiUserService) {
     this.logo = '../../assets/logocovid.png';
     this.background = '../../assets/background1.jpeg';
+    setTimeout(() => {
+      this.admins = this.apiUser.information.filter(u => u.strRol === 'admin');
+    }, 100);
   }
-
+  getAdmin($event) {
+    this.apiUser.getOneUser($event.target.value);
+    setTimeout(() => {
+      console.log(this.apiUser.userInformation);
+    }, 50);
+  }
+  getUser($event) {}
   ngOnInit() {
   }
 
